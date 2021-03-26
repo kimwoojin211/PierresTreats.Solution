a387a4d5920778bc9c010fd72512d6a5e64ae8b9
Woo Jin Kim <kimwoojin211@gmail.com>
2021-03-26 09:40:34 -0700

    build Flavor & Treat Index() Controller and View functionality

---
+++ PierresTreats/Controllers/TreatsController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace PierresTreats.Controllers
+{
+  [Authorize]
+  public class TreatsController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public TreatsController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    [AllowAnonymous]
+    public ActionResult Index()
+    {
+      return View(_db.Treats.ToList());
+    }
+  }
+}
